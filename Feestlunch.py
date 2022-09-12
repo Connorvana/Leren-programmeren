eb9c25cc6fa6ff95e2642f09f743802a33aedf69
Connorvana <99074424@mydavinci.nl>
2022-09-12 09:30:50 +0200

    eerste,module

--- Feestlunch.py
+++ Feestlunch.py
@@ -1,8 +1,13 @@
+
+
+
+from lib2to3.pygram import python_grammar_no_print_statement
+
+
 print(0.39*17)
 
 print(2*2.78)
 
 print(6.63+5.56)
 print(12.19-1.50)
-
-
+print(‘De feestlunch kost je bij de bakker 10.69 euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!’)
