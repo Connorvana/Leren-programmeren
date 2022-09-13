12d3bb53972c65bbacf930c2496e4d931652f3ba
Connorvana <99074424@mydavinci.nl>
2022-09-13 10:19:40 +0200

    2e,module

--- Feestlunch.py
+++ Feestlunch.py
@@ -8,7 +8,9 @@
 croissant= 17
 stokbrood= 2
 
-print((0.39*croissant) + (2.78*stokbrood)-1.50)
+totaal= ((0.39*croissant) + (2.78*stokbrood)-1.50)
+
+print( ' De feestlunch kost je bij de bakker '+ str(totaal) +  'euro voor de '+ str(croissant) + ' croissantjes en de ' + str(stokbrood) + ' stokbroden als de 3 kortingsbonnen nog geldig zijn ')
 
 
 
