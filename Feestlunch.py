566727584a52cf4384e146089147a79dfc3b87de
Connorvana <99074424@mydavinci.nl>
2022-09-13 11:18:26 +0200

    2e,module

--- Feestlunch.py
+++ Feestlunch.py
@@ -10,7 +10,7 @@ stokbrood= 2
 
 totaal= ((0.39*croissant) + (2.78*stokbrood)-1.50)
 
-print( ' De feestlunch kost je bij de bakker '+ str(totaal) +  'euro voor de '+ str(croissant) + ' croissantjes en de ' + str(stokbrood) + ' stokbroden als de 3 kortingsbonnen nog geldig zijn ')
+print( ' De feestlunch kost je bij de bakker '+ str(totaal) +  ' euro voor de '+ str(croissant) + ' croissantjes en de ' + str(stokbrood) + ' stokbroden als de 3 kortingsbonnen nog geldig zijn ')
 
 
 
---
+++ person.py
@@ -0,0 +1,21 @@
+name= 'connor van amersfoort'
+adres= 'kerkplein, 3, uitwijk'
+postcode= '4288JE'
+woonplaats= 'Uitwijk'
+print('____________________')
+print('|' + str (name)  )                         
+print('|' + str (adres) )
+print('|' + str (postcode))
+print('|' + str (woonplaats)) 
+print('|______________________')
+
+
+
+
+
+
+
+
+
+
+ 
