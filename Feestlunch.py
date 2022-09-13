c5a340f5afe37968460ce02ca4c13fe0bbeb8734
Connorvana <99074424@mydavinci.nl>
2022-09-13 11:37:54 +0200

    2e,module

--- Feestlunch.py
+++ Feestlunch.py
@@ -1,16 +1,12 @@
+croissants= input('hoeveel croissants?:')
+print(0.39* int(croissants))
 
+stokbrood= input('Hoeveel stokbroodjes?:')
+print(2.78* int(stokbrood))
 
+totaal= ((0.39* int(croissants)) + (2.78* int(stokbrood)-1.50))
 
-
-
-
-
-croissant= 17
-stokbrood= 2
-
-totaal= ((0.39*croissant) + (2.78*stokbrood)-1.50)
-
-print( ' De feestlunch kost je bij de bakker '+ str(totaal) +  ' euro voor de '+ str(croissant) + ' croissantjes en de ' + str(stokbrood) + ' stokbroden als de 3 kortingsbonnen nog geldig zijn ')
+print( ' De feestlunch kost je bij de bakker '+ str(totaal) +  ' euro voor de '+ str(croissants) + ' croissantjes en de ' + str(stokbrood) + ' stokbroden als de 3 kortingsbonnen nog geldig zijn ')
 
 
 
