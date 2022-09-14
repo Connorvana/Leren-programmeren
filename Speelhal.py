40e6da46cd4cb6436d68b1619cdaa10af3446c00
Connorvana <99074424@mydavinci.nl>
2022-09-14 11:19:23 +0200

    pizza

--- Speelhal.py
+++ Speelhal.py
@@ -1,9 +1,24 @@
-personen= 4
-minuten= 45 
-totaalbedrag= ((7.45*personen) +  (0.074*minuten)   )
-algemeen= (totaalbedrag *  personen )
+from ast import Num
+from multiprocessing.sharedctypes import Value
+from optparse import Values
+from re import X
+from tkinter.tix import INTEGER
+
+
+personen=input('hoeveel personen zijn er?:')
+print(7.45* int(personen))
+
+minuten=input('hoeveel minuten zijn ze bezig?:')
+print(0.074* int(minuten))
+
+totaalbedrag= ((7.45* int(personen)) +  (0.074* int(minuten)))
+algemeen= ( int(totaalbedrag) *  int(personen ))
 
 print (' het bedrag is '+ str(algemeen) + ' euro ' )
+
+
+
+
 
 
 
---
+++ pizzacalculator.py
@@ -0,0 +1,38 @@
+import math
+
+
+small=6
+medium=7.99
+large=9.99
+
+keuze = input('wilt u small, medium of large pizza?: ')
+aantal= input('hoeveel pizzas zou u willen?: ')
+
+prijs= 0 
+
+if keuze == 'small' : 
+    prijs= int(aantal) * small
+if keuze == 'medium' : 
+    prijs= int (aantal) * medium
+if keuze == 'large' : 
+    prijs= int(aantal) * large
+
+print(' __________________________________')
+print('|                                   |')
+print('|', aantal , 'X', keuze ,'pizza     |' )
+print('| U komt op een totaal van''€', prijs,'|' )
+print('------------------------------------')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
