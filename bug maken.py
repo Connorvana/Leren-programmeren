db29ac929a93f9bd646a8417faefce36180114f5
Connorvana <99074424@mydavinci.nl>
2022-10-31 15:22:35 +0100

    update

---
+++ bug maken.py
@@ -0,0 +1,22 @@
+repeat=False
+while repeat == True:
+    leeftijd= print('goedenavond, wat is je leeftijd? ')
+
+    if leeftijd > 18:
+        naam=  print('wat is uw naam?: ')
+        repeat= False
+    else: 
+        print('ga weg')
+        repeat = False
+  
+    gummy=  input('\nwat voor gummybears wil je?  \n1. rood\n2. zwart\n3. geel\n')
+
+    if gummy = '1':
+        print("slechte keuze, je gaat dood")
+    elif gummy = '2':
+        print('waarom zou je zwart snoep kiezen? ')
+    elif gummy = '3':
+        print('fijn dat je voor mij kiest')
+
+
+
--- pizzacalculator.py
+++ pizzacalculator.py
@@ -1,21 +1,35 @@
 import math
+repeat = True
+while repeat == True:
+    keuze = input('wilt u small, medium of large pizza?: ')
+  
+    
+    if keuze == 'small':
+        repeat= False
+        prijs= 5,99
+    
+    elif keuze == 'medium':
+        repeat= False
+        prijs= 7,99
+    
+    elif keuze == 'large': 
+        repeat= False
+        prijs= 8,99
+    else: 
+     print(keuze)
+
+    aantal= input('hoeveel pizzas wilt u? ')
+
+    try: 
+        totaal= int(keuze)* prijs
+    except: 
+        print('dit is geen cijfer')
+        exit()
 
 
-small=6
-medium=7.99
-large=9.99
+    
 
-keuze = input('wilt u small, medium of large pizza?: ')
-aantal= input('hoeveel pizzas zou u willen?: ')
-
-prijs= 0 
-
-if keuze == 'small' : 
-    prijs= int(aantal) * small
-if keuze == 'medium' : 
-    prijs= int (aantal) * medium
-if keuze == 'large' : 
-    prijs= int(aantal) * large
+    
 
 print(' __________________________________')
 print('|                                   |')
