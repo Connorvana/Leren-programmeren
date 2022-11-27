42d4b069aff8ee809c71ece1acdb05e52919475b
Connorvana <99074424@mydavinci.nl>
2022-11-27 16:13:48 +0100

    gimma

--- eigengimma.py
+++ eigengimma.py
@@ -1,49 +1,60 @@
-#mijn game idee: 
-#mijn idee is een 1 speler spel, waarbij je moet raden op welke plek de bom verstopt is
-import random
+repeat=True
 
-repeat= True
-random= random.randint(1,1)
+while repeat == True:
+   
+    nummer = input('Welkom bij deze game| \nKies een nummer: \n A: 1 \n B: 2 \n ')
+   
+    if nummer == 'a':
+        print('Goed geraden! \nWe gaan nu door naar level 2. \n ')
+        level2=input('Level 2| \nKies nu een nummer tussen 1 en 5: \n')
+              
+              #Eerste win
 
-while repeat == True: 
-   
-    nummer= input('kies een nummer tussen 1 en 2 ' )
-   
-    if int(nummer) == int(random) : 
-        print( 'goedzo! je kan doorgaan. ')
-        repeat=True
-   
-    else:
-        print('BOOM! je bent opgeblazen toen je naar een snoepje zocht, maar je hebt nog een leven. ')
-        repeat=True
-   
-    nummer2= input('je bent nu bij level 2| kies een nummer tussen 1 en 2 ')
 
-    if int(nummer) == int(random):
-         print('goedzo je hebt gewonnen! ')
-         quit()
-         
-    else:
-        joker=input('nee dat is fout. Wil je een joker gebruiken?  ja / nee ')
-        repeat=True
-        
-        if joker == 'ja':
-            repeat= True
+        if level2== '2':
+            win = input('Gewonnen! \nWil je opnieuw spelen of stoppen? quit/restart: \n')
+            
+            if win == 'quit':
+                print('bedankt voor het spelen!')
+                repeat=False
+            
+            if win == 'restart':
+                repeat=True
+    #joker ja / nee
+    if nummer == 'b':
+       joker = input('OH! dat was fout :( \nWil je een joker gebruiken? ja/nee \n ')
+
+       if joker == 'nee':
+           opnieuw = input('bedankt voor het spelen! | \nWil je opnieuw spelen of stoppen?  Restart/Quit| \n')
+
+           if opnieuw == 'restart':
+            repeat = True
+
+            if opnieuw == 'quit':
+                print('bedankt voor het spelen')
+                quit()
        
-        if joker == 'nee':
-            quit()
-    
-    nummer3= input('kies een nummer tussen 1 en 2 ')
-    
-    if int(nummer3) ==int(random):
-        print('Goed gespeeld, je hebt gewonnen. ')
-        repeat=False
-    else:
-        print('jeetje wat ben jij slecht zeg!')
-        repeat=False
+       if joker == 'ja':
+            level3 = input('Je hebt nu gekozen om door te gaan naar |LEVEL 3| \nKies nu een nummer tussen 1 en 10 \n ')
+#tweede win
+            if level3 > str(6):
+                win2 = input('Goedzo je hebt gewonnen! \nwil je opnieuw beginnen of wil je stoppen?\n Restart/Quit\n')
 
-        exit()
-        
+                if win2 == 'restart':
+                    repeat=True
+                if win2 == 'quit':
+                    quit()
+
+
+
+
+
+            
+
+
+
+
+
     
     
             
