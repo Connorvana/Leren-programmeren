3a7b2016387f31c06b01fa18b48a0a6159edd4fd
Connorvana <112937994+Connorvana@users.noreply.github.com>
2023-01-10 11:34:37 +0100

    afgemaakt

--- module 3/raadeens.py
+++ module 3/raadeens.py
@@ -1,93 +1,85 @@
-import  random 
+import random
 
-
-ronde= 0
-score = 0 
-
-repeat1= True
-repeat= True
 
 print('Welkom bij dit spel!')
 print('Regels: \n1. \nJe moet in 10 rondes proberen een getal te raden \ntussen de 1 en 1000')
 print('2. \nals je in 10 rondes niet het getal raad dan ben je af en\n stopt het spel\n ')
 print('Dus beginnen maar!')
 
+score = 0
+ronde = 0
+repeat = True
+repeat1 = True
+
 while repeat1 == True:
-    getal = random.randint(1,1000)
-    x = 0 
-    ronde= ronde + 1
+    getal = random.randint(1,1) 
+    print(getal)
+    x = 0
+    ronde = ronde + 1
     repeat = True
-    
-    while repeat== True:
-     
+
+    while repeat == True:
+             
         try:
-         raad_getal=int(input('Welk getal denk je dat het is?\n '))
-         x = x + 1 
-         raad_getal
-         repeat = False
-         rondetwintig = raad_getal - getal
-         
-         if int(raad_getal) > 1000:
-                repeat = True
-                print('heb je geluisterd? het getal moet binnen de 1000 zijn!')
-
-        #getal is lager
-
-         elif raad_getal > getal:
-             print('lager')
-             repeat = True
-             if  (int(rondetwintig)) <= 20:
-                 print('je bent heel warm')
-                 repeat = True
-            
-             elif (int(rondetwintig)) <=50:
-                 print('je bent warm')
-                 repeat = True
-        
-         elif raad_getal < getal:
-             print('Hoger')
-             repeat = True
-            
-             if abs (int(rondetwintig)) <= 20:
-                 print('je bent heel warm')
-                 repeat = True
-            
-             elif abs (int(rondetwintig)) <=50:
-                 print('je bent warm')
-                 repeat = True
-         
-         elif raad_getal == getal:
-             score = score + 1 
-             print('Dat is goed \n'+ 'je score is: ' + str(score))
-             opnieuw = input('Nog een getal raden?  ja/nee\n').lower()
-
-             if opnieuw == 'ja':
-                repeat1= True
-                if (int(ronde)) == 3:
-                    print('Balen! je rondes zijn voorbij! \n Game over \n je score is: ' + str(score))
-                    quit()
-             elif opnieuw == 'nee':
-                print('Bedankt voor het spelen!\nLatersss \nJe score was: ' + str(score))
-                quit()
-        
-         if int(x) == 10:
+            geraden_getal = int(input("Wat denk je dat het getal is? : "))
+            x = x + 1
+            geraden_getal
             repeat = False
-            print( 'Helaas! Je mag niet meer raden. Je score is: ' + str(score))
+            roundtwintig = geraden_getal - getal
 
-            opnieuw = input('Nog een getal raden? Ja/nee \n').lower() 
+            if int(geraden_getal) > 1000 :
+                repeat = True
+                print("Heb je geluisterd? het getal moet binnen de duizend zijn!!!")
+
+            # ingevoerde getal is lager
+            elif geraden_getal > getal:
+                print("Lager!")
+                repeat = True
+               
+            elif geraden_getal < getal:
+                print("Hoger!")
+                repeat = True
+                if abs(int(roundtwintig)) <= 20:
+                    print("Je bent heel warm!")
+                    repeat = True
+                elif abs(int(roundtwintig)) <= 50:
+                    print("Je bent warm!")
+                    repeat = True
             
-            if opnieuw == 'ja':
-                repeat1= True
-                if int(ronde) == 20:
-                    print('Sorry je rondes zijn voorbij! \nGame over! \n' + str(score))
+            elif geraden_getal == getal:
+                score = score + 1 
+                print("Dat is juist!\n" +"Jouw score : "+ str(score))
+                opnieuw = input("Nog een getal raden? [Ja/Nee]\n").lower()
+                
+                if opnieuw == "ja":
+                    repeat1 = True
+                    if int(ronde) == 20:
+                        print("Helaas zijn je ronde's voorbij\nGame Over!\n" + "Jouw score: " + str(score))
+                        quit()
+                    
+                elif opnieuw == "nee":
+                    print("Bedankt voor spelen!\nTot ziens")
                     quit()
-            elif opnieuw == 'nee': 
-                print('Bedankt voor het spelen! \nTot ziens!')
-                quit()
-        
-        except Exception as error:
-            print('dat is geen nummer!')
+            
+            if int(x) == 10:
+                repeat = False
+                print("Helaas zijn je pogingen voorbij\n" + "Jouw score : " + str(score))
+            
+                opnieuw = input("Nog een getal raden? [Ja/Nee]\n").lower()
+                if opnieuw == "ja":
+                    repeat1 = True
+                    if int(ronde) == 20:
+                        print("Helaas zijn je ronde's voorbij\nGame Over!\n" + "Jouw score: " + str(score))
+                        quit()
+    
+                elif opnieuw == "nee":
+                    print("Bedankt voor spelen!\nTot latersss!")
+                    quit()
 
+            
+
+        except Exception as error:
+            print("Dat is geen getal! ")  
     
     
     
---
+++ module 4/speelkaarten.py
@@ -0,0 +1,12 @@
+import random
+kaarten= ['harten 2','harten 3', 'harten 4', 'harten 5', 'harten 6', 'harten 7', 'harten 8', 'harten 9', 'harten 10', 
+'harten boer','harten vrouw', 'harten koning', 'harten aas','schoppen 2', 'schoppen 3', 'schoppen 4', 'schoppen 5', 'schoppen 6', 
+'schoppen 7', 'schoppen 8', 'schoppen 9', 'schoppen 10','schoppen boer', 'schoppen vrouw','schoppen koning', 'schoppen aas','klaver 2', 
+'klaver 3', 'klaver 4', 'klaver 5','klaver 6', 'klaver 7','klaver 8','klaver 9', 'klaver 10', 'klaver boer','klaver vrouw', 
+'klaver koning', 'klaver aas','ruiten 2', 'ruiten 3', 'ruiten 4', 'ruiten 5', 'ruiten 6', 'ruiten 7', 'ruiten 8', 
+'ruiten 9', 'ruiten 10', 'ruiten boer', 'ruiten vrouw', 'ruiten koning', 'ruiten aas', 'joker', 'joker']
+
+random= random.shuffle(kaarten)
+print('Kaart 1:'(str + kaarten[0:1]))
+print('Kaart 2:'(kaarten[1:2]))
+print(kaarten)
